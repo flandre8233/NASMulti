69075a7bdaa74428a3c99fe17c2ac4cfb6ee6cfc
Gibson's macbook2019 <>
2021-09-29 12:11:50 +0800

    no message

--- Assets/DevGameTools/Assets/GibStuff/Common/CommonUIButton.cs
+++ Assets/DevGameTools/Assets/GibStuff/Common/CommonUIButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public abstract class CommonUIButton : MonoBehaviour
 {
-    protected Button button;
+    public Button button;
 
     // Start is called before the first frame update
     protected virtual void Start()
